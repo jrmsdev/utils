3ff5b65a03b2452c257cfc045f4506b784f1a30f
Jeremías <jeremiasc@gmail.com>
2026-07-03 17:10:59 -0300

    improve brew backups

--- sbin/brew-backups.sh
+++ sbin/brew-backups.sh
@@ -2,8 +2,15 @@
 set -eu
 brew="${HOME}/Utils/bin/brew.sh"
 bups="${HOME}/Backups/brew"
+if test -d "${bups}.prev"; then
+    rm -rf "${bups}.prev"
+fi
+if test -d "${bups}"; then
+    mv -v "${bups}" "${bups}.prev"
+fi
 mkdir -vp "${bups}"
 set -x
 $brew config | tee "${bups}/brew.config"
 $brew list --installed-on-request | tee "${bups}/brew.install"
 $brew list --cask | tee -a "${bups}/brew.install"
+$brew bundle dump --file "${bups}/Brewfile"
