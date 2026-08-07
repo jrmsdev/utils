33008bed9aec51190e54a6dac06d88db8dea7d1a
Jeremías <jeremiasc@gmail.com>
2026-08-07 18:36:38 -0300

    brew upgrade call cleanup

--- sbin/brew-upgrade.sh
+++ sbin/brew-upgrade.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -eu
 brew="${HOME}/Utils/bin/brew.sh"
+brew_cleanup="${HOME}/Utils/sbin/brew-cleanup.sh"
 set -x
 $brew update
 $brew outdated --greedy
 $brew upgrade --greedy --yes
+$brew_cleanup
